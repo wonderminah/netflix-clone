b05aeeb6c96390d4d2d32255bc52f88e150b6d81
Minah Kim <kimma1205@gmail.com>
2026-03-02 17:33:44 +0900

    movie 타입 추가

---
+++ src/types/movie.ts
@@ -0,0 +1,9 @@
+export interface Movie {
+    id: number
+    title?: string
+    name?: string
+    overview: string
+    backdrop_path: string
+    poster_path: string
+    vote_average: number
+}
